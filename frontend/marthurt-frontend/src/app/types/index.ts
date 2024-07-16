7a8bdd43abcaa7b28e207a22270d36689c4fe611
s28490 <s28490@pjwstk.edu.pl>
2024-07-16 13:12:14 +0200

    add footer

--- frontend/marthurt-frontend/src/app/types/index.ts
+++ frontend/marthurt-frontend/src/app/types/index.ts
@@ -1,20 +1,32 @@
 export interface Option {
-  value: string;
-  label: string;
+	value: string
+	label: string
 }
 
 export interface CustomSelectProps {
-  options: Option[];
-  onChange: (selectedOption: Option) => void;
-  placeholder?: string;
+	options: Option[]
+	onChange: (selectedOption: Option) => void
+	placeholder?: string
 }
 
 export interface FormData {
-  name: string;
-  photo?: File | null;
-  description: string;
-  bruttoPrice: number;
-  bruttoPriceWithDiscount: number;
-  quantity: number;
-  totalValueAfterDiscount: number;
+	name: string
+	photo?: File | null
+	description: string
+	bruttoPrice: number
+	bruttoPriceWithDiscount: number
+	quantity: number
+	totalValueAfterDiscount: number
+}
+
+export interface Quote {
+	store: string
+	date: string
+	salesPerson: string
+	items: FormData[]
+	clientName: string
+	clientEmail: string
+	clientPhone: string
+	deliveryDate: string
+	additionalInfo: string
 }
