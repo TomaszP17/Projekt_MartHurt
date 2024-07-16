e30126a5734e6136d5d932660c4dbd5fc6c72224
Kuba0517 <jakub.ratusz@gmail.com>
2024-07-16 12:27:48 +0200

    Add shadcn library with an example in productGrid

--- frontend/marthurt-frontend/src/app/layout.tsx
+++ frontend/marthurt-frontend/src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>Navbar</h1>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
