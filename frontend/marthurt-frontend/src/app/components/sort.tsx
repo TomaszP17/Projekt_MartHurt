a28e1b1018fbfe5e5879775c16f12df5cab64f51
s28490 <s28490@pjwstk.edu.pl>
2024-07-18 13:25:34 +0200

    update productsGrid

--- frontend/marthurt-frontend/src/app/components/sort.tsx
+++ frontend/marthurt-frontend/src/app/components/sort.tsx
@@ -1,31 +1,32 @@
 // src/components/Sort.tsx
-"use client";
-import React from 'react';
-import { useSortStore } from '@/store/useFilterStore';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger } from '@/components/ui/select';
+'use client'
+import React from 'react'
+import { useSortStore } from '@/store/useFilterStore'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select'
 
 const Sort: React.FC = () => {
-  const { sortBy, setSortBy } = useSortStore();
+	const { sortBy, setSortBy } = useSortStore()
 
-  const handleSortByChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSortBy(event.target.value);
-  };
+	const handleSortByChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setSortBy(event.target.value)
+	}
 
-  return (
-    <div className='flex flex-col gap-4'>
-      <Select>
-        <SelectTrigger className='w-[180px]'>
-          <SelectContent>
-            <SelectGroup>
-              <SelectLabel>Sortowanie</SelectLabel>
-              <SelectItem value='oldest'>Od najstarszych</SelectItem>
-              <SelectItem value='newest'>Od najnowszych</SelectItem>
-            </SelectGroup>
-          </SelectContent>
+	return (
+		<div className='flex flex-col gap-4'>
+			<Select>
+				<SelectTrigger className='w-[180px]'>
+        <SelectValue placeholder="Sortuj" />
         </SelectTrigger>
-      </Select>
-    </div>
-  );
-};
+				<SelectContent>
+					<SelectGroup>
+						<SelectLabel>Sortowanie</SelectLabel>
+						<SelectItem value='oldest'>Od najstarszych</SelectItem>
+						<SelectItem value='newest'>Od najnowszych</SelectItem>
+					</SelectGroup>
+				</SelectContent>
+			</Select>
+		</div>
+	)
+}
 
-export default Sort;
+export default Sort
