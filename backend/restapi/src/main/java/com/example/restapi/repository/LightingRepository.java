24d3c10c3452171848f5afb2ba175f63c2f42738
TomaszP17 <tplucinski15@gmail.com>
2024-07-23 11:54:36 +0200

    Added news from lighting

--- backend/restapi/src/main/java/com/example/restapi/repository/LightingRepository.java
+++ backend/restapi/src/main/java/com/example/restapi/repository/LightingRepository.java
@@ -1,10 +1,12 @@
 package com.example.restapi.repository;
 
 import com.example.restapi.entity.products.Lighting;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LightingRepository extends JpaRepository<Lighting, String> {
-
-
-
+    @Query("SELECT l FROM Lighting l JOIN l.product p ORDER BY p.dateAdded DESC")
+    Page<Lighting> findTopByOrderByProductDateAddedDesc(Pageable pageable);
 }
