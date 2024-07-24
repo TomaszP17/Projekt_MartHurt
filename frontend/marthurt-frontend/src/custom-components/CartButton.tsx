e069e4aa1f9949bd68ac6084cbbad6ae0b6f551c
s28490 <s28490@pjwstk.edu.pl>
2024-07-24 10:45:54 +0200

    add CartSlide

--- frontend/marthurt-frontend/src/custom-components/CartButton.tsx
+++ frontend/marthurt-frontend/src/custom-components/CartButton.tsx
@@ -1,37 +1,33 @@
-"use client";
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import { useCartStore } from '@/store/useCartStore'
 
-import React from "react";
-import { Button } from "@/components/ui/button";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@/components/ui/hover-card";
-import { useCartStore } from "@/store/useCartStore";
+interface CartButtonProps {
+	toggleCart: () => void
+}
 
-export function CartButton() {
-  const { totalItems } = useCartStore();
+export function CartButton({ toggleCart }: CartButtonProps) {
+	const { totalItems } = useCartStore()
 
-  return (
-    <HoverCard>
-      <HoverCardTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-        >
-          <span className="material-symbols-outlined h-6 w-6">
-            shopping_bag
-          </span>
-          <span className="ml-1">Koszyk ({totalItems})</span>
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent className="w-64">
-        <div className="p-4">
-          <h4 className="text-sm font-semibold">Koszyk</h4>
-          <p className="text-sm">Masz {totalItems} przedmiotów w koszyku.</p>
-        </div>
-      </HoverCardContent>
-    </HoverCard>
-  );
+	return (
+		<HoverCard>
+			<HoverCardTrigger asChild>
+				<Button
+					onClick={toggleCart}
+					variant='ghost'
+					size='icon'
+					className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50'>
+					<span className='material-symbols-outlined h-6 w-6'>shopping_bag</span>
+					<span className='ml-1'>Koszyk ({totalItems})</span>
+				</Button>
+			</HoverCardTrigger>
+			<HoverCardContent className='w-64'>
+				<div className='p-4'>
+					<h4 className='text-sm font-semibold'>Koszyk</h4>
+					<p className='text-sm'>Masz {totalItems} przedmiotów w koszyku.</p>
+				</div>
+			</HoverCardContent>
+		</HoverCard>
+	)
 }
--- frontend/marthurt-frontend/src/custom-components/navbar.tsx
+++ frontend/marthurt-frontend/src/custom-components/navbar.tsx
@@ -1,150 +1,238 @@
-"use client";
-import React, { useEffect } from "react";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { Input } from "@/components/ui/input";
-import { useCartStore } from "@/store/useCartStore";
-import { CartButton } from "./CartButton";
-import { ShoppingCart } from "lucide-react";
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { Input } from '@/components/ui/input'
+import { useCartStore } from '@/store/useCartStore'
+import { CartButton } from './CartButton'
+import { Dialog } from '@headlessui/react'
 
 export default function Navbar() {
-  const { totalItems } = useCartStore();
+	const { totalItems } = useCartStore()
+	const [isCartOpen, setIsCartOpen] = useState(false)
 
-  useEffect(() => {
-    const link = document.createElement("link");
-    link.href =
-      "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"; // ni
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
+	const toggleCart = () => {
+		setIsCartOpen(prev => !prev)
+	}
 
-    return () => {
-      document.head.removeChild(link);
-    };
-  }, []);
+	useEffect(() => {
+		const link = document.createElement('link')
+		link.href =
+			'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'
+		link.rel = 'stylesheet'
+		document.head.appendChild(link)
 
-  return (
-    <>
-      <header className="flex h-20 w-full shrink-0 items-center px-6">
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon" className="lg:hidden">
-              <MenuIcon className="h-6 w-6" />
-              <span className="sr-only">Toggle navigation menu</span>
-            </Button>
-          </SheetTrigger>
-          <SheetContent side="left">
-            <div className="grid gap-2 py-6">
-              <Link
-                href="#"
-                className="flex w-full items-center py-2 text-lg font-semibold"
-                prefetch={false}
-              >
-                Strona Główna
-              </Link>
-              <Link
-                href="#"
-                className="flex w-full items-center py-2 text-lg font-semibold"
-                prefetch={false}
-              >
-                O nas
-              </Link>
-              <Link
-                href="#"
-                className="flex w-full items-center py-2 text-lg font-semibold"
-                prefetch={false}
-              >
-                Kontakt
-              </Link>
-            </div>
-          </SheetContent>
-        </Sheet>
-        <Link href="/" className="mr-6 hidden lg:flex" prefetch={false}>
-          <h1 className="text-2xl font-bold text-gray-900">MART-HURT</h1>
-        </Link>
-        <nav className="hidden lg:flex lg:items-center lg:gap-6">
-          <Link
-            href="#"
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-            prefetch={false}
-          >
-            Strona Główna
-          </Link>
-          <Link
-            href="#"
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-            prefetch={false}
-          >
-            O nas
-          </Link>
-          <Link
-            href="#"
-            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-            prefetch={false}
-          >
-            Kontakt
-          </Link>
-        </nav>
-        <div className="flex items-center gap-10 ml-auto">
-          <div className="relative">
-            <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Szukaj..."
-              className="w-full rounded-lg bg-background pl-8 pr-4 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary"
-            />
-          </div>
-          <CartButton />
-          <Button
-            variant={`outline`}
-            className="px-4 py-2 rounded-md hover:bg-black hover:text-white"
-          >
-            Zaloguj się
-          </Button>
-        </div>
-      </header>
-      <hr className="border-gray-300" />
-    </>
-  );
+		return () => {
+			document.head.removeChild(link)
+		}
+	}, [])
+
+	return (
+		<>
+			<header className='flex h-20 w-full shrink-0 items-center px-6'>
+				<Sheet>
+					<SheetTrigger asChild>
+						<Button variant='outline' size='icon' className='lg:hidden'>
+							<MenuIcon className='h-6 w-6' />
+							<span className='sr-only'>Toggle navigation menu</span>
+						</Button>
+					</SheetTrigger>
+					<SheetContent side='left'>
+						<div className='grid gap-2 py-6'>
+							<Link href='#' className='flex w-full items-center py-2 text-lg font-semibold' prefetch={false}>
+								Strona Główna
+							</Link>
+							<Link href='#' className='flex w-full items-center py-2 text-lg font-semibold' prefetch={false}>
+								O nas
+							</Link>
+							<Link href='#' className='flex w-full items-center py-2 text-lg font-semibold' prefetch={false}>
+								Kontakt
+							</Link>
+						</div>
+					</SheetContent>
+				</Sheet>
+				<Link href='/' className='mr-6 hidden lg:flex' prefetch={false}>
+					<h1 className='text-2xl font-bold text-gray-900'>MART-HURT</h1>
+				</Link>
+				<nav className='hidden lg:flex lg:items-center lg:gap-6'>
+					<Link
+						href='#'
+						className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50'>
+						Strona Główna
+					</Link>
+					<Link
+						href='#'
+						className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50'>
+						O nas
+					</Link>
+					<Link
+						href='#'
+						className='group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50'>
+						Kontakt
+					</Link>
+				</nav>
+				<div className='flex items-center gap-10 ml-auto'>
+					<div className='relative'>
+						<SearchIcon className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
+						<Input
+							type='search'
+							placeholder='Szukaj...'
+							className='w-full rounded-lg bg-background pl-8 pr-4 py-2 text-sm focus:outline-none focus:ring-1 focus:ring-primary'
+						/>
+					</div>
+					{/* CartButton now correctly toggles the cart */}
+					<CartButton toggleCart={toggleCart} />
+					<Link href='/register'>
+						<Button variant='outline' className='px-4 py-2 rounded-md hover:bg-black hover:text-white'>
+							Zaloguj się
+						</Button>
+					</Link>
+				</div>
+			</header>
+			<hr className='border-gray-300' />
+
+			{/* Dialog to display cart */}
+			<Dialog open={isCartOpen} onClose={toggleCart} className='relative z-10'>
+				<div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' aria-hidden='true'></div>
+				<div className='fixed inset-0 overflow-hidden'>
+					<div className='absolute inset-0 overflow-hidden'>
+						<div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10'>
+							<div className='pointer-events-auto w-screen max-w-md'>
+								<div className='flex h-full flex-col overflow-y-scroll bg-white shadow-xl'>
+									<div className='flex-1 overflow-y-auto px-4 py-6 sm:px-6'>
+										<div className='flex items-start justify-between'>
+											<h2 className='text-lg font-medium text-gray-900'>Koszyk</h2>
+											<div className='ml-3 flex h-7 items-center'>
+												<button
+													type='button'
+													className='bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+													onClick={toggleCart}>
+													<span className='sr-only'>Close panel</span>
+													<svg
+														className='h-6 w-6'
+														xmlns='http://www.w3.org/2000/svg'
+														fill='none'
+														viewBox='0 0 24 24'
+														stroke='currentColor'
+														aria-hidden='true'>
+														<path
+															strokeLinecap='round'
+															strokeLinejoin='round'
+															strokeWidth='2'
+															d='M6 18L18 6M6 6l12 12'
+														/>
+													</svg>
+												</button>
+											</div>
+										</div>
+										<div className='mt-8'>
+											<div className='flow-root'>
+												<ul role='list' className='-my-6 divide-y divide-gray-200'>
+													<li className='flex py-6'>
+														<div className='h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200'>
+															<img
+																src='https://via.placeholder.com/150'
+																alt='Product Image'
+																className='h-full w-full object-cover object-center'
+															/>
+														</div>
+														<div className='ml-4 flex flex-1 flex-col'>
+															<div>
+																<div className='flex justify-between text-base font-medium text-gray-900'>
+																	<h3>
+																		<a href='#'>Product Name</a>
+																	</h3>
+																	<p className='ml-4'>$50.00</p>
+																</div>
+																<p className='mt-1 text-sm text-gray-500'>Product Color</p>
+															</div>
+															<div className='flex flex-1 items-end justify-between text-sm'>
+																<p className='text-gray-500'>Qty 1</p>
+																<div className='flex'>
+																	<button type='button' className='font-medium text-indigo-600 hover:text-indigo-500'>
+																		Remove
+																	</button>
+																</div>
+															</div>
+														</div>
+													</li>
+												</ul>
+											</div>
+										</div>
+									</div>
+									<div className='border-t border-gray-200 px-4 py-6 sm:px-6'>
+										<div className='flex justify-between text-base font-medium text-gray-900'>
+											<p>Subtotal</p>
+											<p>$50.00</p>
+										</div>
+										<p className='mt-0.5 text-sm text-gray-500'>Shipping and taxes calculated at checkout.</p>
+										<div className='mt-6'>
+											<a
+												href='#'
+												className='flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700'>
+												Checkout
+											</a>
+										</div>
+										<div className='mt-6 flex justify-center text-center text-sm text-gray-500'>
+											<p>
+												or
+												<button
+													type='button'
+													className='font-medium text-indigo-600 hover:text-indigo-500'
+													onClick={toggleCart}>
+													Continue Shopping
+													<span aria-hidden='true'> &rarr;</span>
+												</button>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</Dialog>
+		</>
+	)
 }
 
 function MenuIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <line x1="4" x2="20" y1="12" y2="12" />
-      <line x1="4" x2="20" y1="6" y2="6" />
-      <line x1="4" x2="20" y1="18" y2="18" />
-    </svg>
-  );
+	return (
+		<svg
+			{...props}
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'>
+			<line x1='4' x2='20' y1='12' y2='12' />
+			<line x1='4' x2='20' y1='6' y2='6' />
+			<line x1='4' x2='20' y1='18' y2='18' />
+		</svg>
+	)
 }
 
 function SearchIcon(props: any) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
-    </svg>
-  );
+	return (
+		<svg
+			{...props}
+			xmlns='http://www.w3.org/2000/svg'
+			width='24'
+			height='24'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'>
+			<circle cx='11' cy='11' r='8' />
+			<path d='m21 21-4.3-4.3' />
+		</svg>
+	)
 }
