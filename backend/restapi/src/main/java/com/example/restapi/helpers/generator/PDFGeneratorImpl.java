0afd221eda4624d976e5294b09bdc80644379cf9
TomaszP17 <tplucinski15@gmail.com>
2024-07-27 10:00:18 +0200

    edit pdf generator

--- backend/restapi/src/main/java/com/example/restapi/helpers/generator/PDFGeneratorImpl.java
+++ backend/restapi/src/main/java/com/example/restapi/helpers/generator/PDFGeneratorImpl.java
@@ -26,8 +26,8 @@ public class PDFGeneratorImpl implements IPDFGenerator {
             PDPage firstPage = new PDPage(PDRectangle.A4);
             document.addPage(firstPage);
 
-            String name = "Chandan Kumar";
-            String callNo = "+91-8777666555";
+            String name = "Marta Marthurt";
+            String callNo = "+48 606401620";
 
             Format d_format = new SimpleDateFormat("dd/MM/yyyy");
             Format t_format = new SimpleDateFormat("HH:mm");
@@ -46,21 +46,21 @@ public class PDFGeneratorImpl implements IPDFGenerator {
                 myTextClass.addMultiLineText(contactDetails, 18, (int) (pageWidth - font.getStringWidth("9876543210") / 1000 * 15 - 10),
                         pageHeight - 25, font, 15, Color.BLACK);
 
-                myTextClass.addSingleLineText("INDIAN TADKA", 25, pageHeight - 150, font, 40, Color.BLACK);
-                myTextClass.addSingleLineText("Customer Name: " + name, 25, pageHeight - 250, font, 16, Color.BLACK);
-                myTextClass.addSingleLineText("Mo. No: " + callNo, 25, pageHeight - 274, font, 16, Color.BLACK);
+                myTextClass.addSingleLineText("MARTHURT", 25, pageHeight - 150, font, 40, Color.BLACK);
+                myTextClass.addSingleLineText("Imie Klienta: " + name, 25, pageHeight - 250, font, 16, Color.BLACK);
+                myTextClass.addSingleLineText("Nr. Tel: " + callNo, 25, pageHeight - 274, font, 16, Color.BLACK);
 
-                String invoiceNo = "Invoice# 2536";
+                String invoiceNo = "ID wyceny# 2536";
                 float textWidth = myTextClass.getTextWidth(invoiceNo, font, 16);
                 myTextClass.addSingleLineText(invoiceNo, (int) (pageWidth - 25 - textWidth), pageHeight - 250, font, 16, Color.BLACK);
 
-                float dateTextWidth = myTextClass.getTextWidth("Date: " + d_format.format(new Date()), font, 16);
-                myTextClass.addSingleLineText("Date: " + d_format.format(new Date()), (int) (pageWidth - 25 - dateTextWidth),
+                float dateTextWidth = myTextClass.getTextWidth("Data: " + d_format.format(new Date()), font, 16);
+                myTextClass.addSingleLineText("Data: " + d_format.format(new Date()), (int) (pageWidth - 25 - dateTextWidth),
                         pageHeight - 274, font, 16, Color.BLACK);
 
                 String time = t_format.format(new Date());
-                float timeTextWidth = myTextClass.getTextWidth("Time: " + time, font, 16);
-                myTextClass.addSingleLineText("Time: " + time, (int) (pageWidth - 25 - timeTextWidth),
+                float timeTextWidth = myTextClass.getTextWidth("Godzina: " + time, font, 16);
+                myTextClass.addSingleLineText("Godzina: " + time, (int) (pageWidth - 25 - timeTextWidth),
                         pageHeight - 298, font, 16, Color.BLACK);
 
                 MyTableClass myTable = new MyTableClass(document, contentStream);
@@ -71,20 +71,20 @@ public class PDFGeneratorImpl implements IPDFGenerator {
                 Color tableHeadColor = new Color(240, 93, 11);
                 Color tableBodyColor = new Color(219, 218, 198);
 
-                myTable.addCell("Si.No.", tableHeadColor);
-                myTable.addCell("Items", tableHeadColor);
-                myTable.addCell("Price", tableHeadColor);
-                myTable.addCell("Qty", tableHeadColor);
-                myTable.addCell("Total", tableHeadColor);
+                myTable.addCell("l.p.", tableHeadColor);
+                myTable.addCell("Produkt", tableHeadColor);
+                myTable.addCell("Cena", tableHeadColor);
+                myTable.addCell("Ilosc", tableHeadColor);
+                myTable.addCell("Lacznie", tableHeadColor);
 
                 myTable.addCell("1. ", tableBodyColor);
-                myTable.addCell("Masala Dosa", tableBodyColor);
+                myTable.addCell("Plafon Splendor 40 Gold", tableBodyColor);
                 myTable.addCell("120", tableBodyColor);
                 myTable.addCell("2", tableBodyColor);
                 myTable.addCell("240", tableBodyColor);
 
-                myTable.addCell("3. ", tableBodyColor);
-                myTable.addCell("Dhokla", tableBodyColor);
+                myTable.addCell("2. ", tableBodyColor);
+                myTable.addCell("Lampa wiszaca Splendor 60 Gold", tableBodyColor);
                 myTable.addCell("50", tableBodyColor);
                 myTable.addCell("4", tableBodyColor);
                 myTable.addCell("200", tableBodyColor);
@@ -101,7 +101,7 @@ public class PDFGeneratorImpl implements IPDFGenerator {
                 myTable.addCell("", null);
                 myTable.addCell("462", null);
 
-                String[] paymentMethod = {"Methods of payment we accept:", "Cash, PhonePe, GPay, RuPay, Meastro", "Visa, Mastercard and American Express"};
+                String[] paymentMethod = {"Metody platnosci jakie akceptujemy:", "Gotowka, platnosc karta lub BLIKiem"};
                 myTextClass.addMultiLineText(paymentMethod,
                         15,
                         25,
@@ -116,12 +116,12 @@ public class PDFGeneratorImpl implements IPDFGenerator {
                 contentStream.lineTo(pageWidth - 25, 150);
                 contentStream.stroke();
 
-                String authoSign = "Authorised Signatory";
+                String authoSign = "Podpis Handlowca";
                 float authoSignWidth = myTextClass.getTextWidth(authoSign, italicFont, 16);
                 int xpos = pageWidth - 250 + pageWidth - 25;
                 myTextClass.addSingleLineText(authoSign, (int) (xpos - authoSignWidth) / 2, 125, italicFont, 16, Color.BLACK);
 
-                String bottomLine = "Rain or shine, it's time to dine";
+                String bottomLine = "Tutaj miejsce na jakas ciekawa stopke";
                 float bottomLineWidth = myTextClass.getTextWidth(bottomLine, font, 20);
                 myTextClass.addSingleLineText(bottomLine, (int) (pageWidth - bottomLineWidth) / 2, 50, italicFont, 20, Color.GRAY);
 
